04f56c9438515de3833a5f2832497b8e740e9e88
mlandale1 <Matthew.Landale1@marist.edu>
2019-09-20 19:47:35 -0400

    Solution to Mad Libs exercise.

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+def main():
+    noun = input("Enter a noun:")
+    adjective = input("Enter an adjective:")
+    verb = input("Enter a verb:")
+    place = input("Enter a place:")
+    
+    print('I take my {0} {1} and {2} at my friends {3}'.format(adjective, noun, verb, place))
+     
+main()
+    
