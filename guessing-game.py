1e0fa461d469d861ecd087007e0998a9ade55b7a
mlandale1 <Matthew.Landale1@marist.edu>
2019-10-18 20:19:15 -0400

    Initial version of a simple guessing game.

---
+++ guessing-game.py
@@ -0,0 +1,13 @@
+#guessing game
+#10/16/2019
+#Matt Landale
+
+def isTrue(prompt):
+    animal = "lion"
+
+    if prompt == animal:
+
+        return "Correct!"
+    else:
+        return "Wrong!"
+
