ee6f029d1ffaa29a863d02acb62834ec0cb9069e
mlandale1 <Matthew.Landale1@marist.edu>
2019-10-18 22:13:31 -0400

    Usability improvements to the guessing game, resolves #M and #N.

--- guessing-game.py
+++ guessing-game.py
@@ -2,9 +2,8 @@
 #10/16/2019
 #Matt Landale
 
-import random
+animal = 'lion'
 
-animal = "lion"
 
 print("Lets play a guessing game!")
             
@@ -12,11 +11,31 @@ while True:
         print("I am thinking of a random animal. Try to guess the animal!")
         prompt = input("Guess: ")
         animal = "lion"
-
+       
         if prompt == animal:
-            print("Correct!")
-            break
+                print("Correct!")
+                break
         else:
-            print("Incorrect!")
-            
+                print("Incorrect!")
+
+def main():
+    while continue_game():
+        continue_game()
+
+def continue_game
+():
+
+    answer = input("Do you wish to continue?: ")
+
+    if answer == "yes":
+        print("You have chosen to continue on")
+        return True
+    else:
+        print("You have chosen to quit this program")
+        return False
+
+if __name__ == "__main__":
+    main()
+
+
 
