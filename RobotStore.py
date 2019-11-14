b8e781cb22187d6c4b1e50120a6bc6a49d77868a
mlandale1 <Matthew.Landale1@marist.edu>
2019-11-14 02:29:30 -0500

    Modified Robot Store, couldn't figure how to add total cost and product in stock.

--- RobotStore.py
+++ RobotStore.py
@@ -2,42 +2,39 @@
 # Lab 8
 # 15 November 2019
 
-productNames =  [ "Ultrasonic range finder"                , "Servo motor"                , "Servo controller"                , "Microcontroller Board"                , "Laser range finder"                , "Lithium polymer battery"                ] 
-productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-productQuantities = [ 4, 10, 5, 7, 2, 8 ]
-
-def printStock():
+def main():
     print()
     print("Available Products")
     print("------------------")
-    for i in range(0,len(productNames)):
-        if productQuantities[i] > 0:
-            print(str(i)+")",productNames[i], "$", productPrices[i])
-    print()
-
-def main():
-    cash = float(input("How much money do you have? $"))
-    while cash > 0:
-        printStock()
-
-        vals = input("Enter product ID and quantity you wish to buy: ").split(" ")
-
-
-        if vals[0] == "quit": break
-
-        prodId = int(vals[0])
-        count = int(vals[1])
-
-        if productQuantities[prodId] >= count:
-            if cash >= productPrices[prodId]:
-                productQuantities[prodId] -= count
-                cash -= productPrices[prodId] * count
-                print("You purchased", count, productNames[prodId]+".")
-                print("You have $", "{0:.2f}".format(cash), "remaining.")
-            else:
-                print("Sorry, you cannot afford that product.")
-        else:
-            print("Sorry, we are sold out of", productNames[prodId])
 main()
-        
-                        
+
+cash = float(input("How much money do you have: "))
+while cash > 0:
+    
+
+print("------------------")
+ 
+class RobotStore:
+    
+    def __init__(self):  
+        self.Ultrasonic_range_finder = "Ultrasonic range finder [$2.50] [Q:4]"
+        self.Servo_motor = "Servo motor [$14.99] [Q:10]"
+        self.Servo_controller = "Servo controller [$44.95] [Q:10]"
+        self.Microcontroller_Board = "Microcontroller Board [$34.95] [Q:7]"
+        self.Laser_range_finder = "Laser range finder [$149.99] [Q:2]"
+        self.Lithium_polymer_battery = "Lithium polymer battery [$8.99] [Q:8]"
+
+    def print_RobotStore(self):
+        print(self.Ultrasonic_range_finder)
+        print(self.Servo_motor)
+        print(self.Servo_controller)
+        print(self.Microcontroller_Board)
+        print(self.Laser_range_finder)
+        print(self.Lithium_polymer_battery)
+
+obj = RobotStore()
+
+obj.print_RobotStore()
+
+
+
