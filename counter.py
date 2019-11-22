5ebbf6512f92575bf867cc111b4e148e2573f509
mlandale1 <Matthew.Landale1@marist.edu>
2019-11-22 15:46:31 -0500

    Decrementing counter 2.

--- counter.py
+++ counter.py
@@ -1,26 +1,11 @@
 # Matt Landale
 # Lab 9
 
-class DecrementingCounter:
+class DecrementingCounter2(Counter):
     """Simple counter that can be incremented, decremented, and cleared.""" 
- 
-    def __init__(self):
-        """Initialize counter to 0."""
-        self.count = 0 
- 
-    def increment(self):
-        """Increment counter by 1."""
-        self.count += 1 
  
     def decrement(self):
         """Decrement counter by 1."""
-        self.count -= 1
-        
-    def clear(self):
-        """Clear counter to 0."""
-        self.count = 0 
+        self.count -= 1 
  
-    def get_value(self):
-        """Return the current value of the counter."""
-        return self.count 
  
