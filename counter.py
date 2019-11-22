9c0a65afbb6d0d695aa47af880aba53cea78b17f
mlandale1 <Matthew.Landale1@marist.edu>
2019-11-22 15:47:24 -0500

    Override

--- counter.py
+++ counter.py
@@ -4,8 +4,11 @@
 class DecrementingCounter2(Counter):
     """Simple counter that can be incremented, decremented, and cleared.""" 
  
+    def increment(self):
+        """Increment counter by 2."""
+        self.count += 2 
+ 
     def decrement(self):
         """Decrement counter by 1."""
         self.count -= 1 
- 
  
